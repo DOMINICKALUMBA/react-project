f77e644c493379fad480a3f7d7994e66771eb395
DOMINIC KALUMBA <dominiquekalumba1000@gmail.com>
2025-02-02 17:16:33 +0300

    react

---
+++ src/valid.js
@@ -0,0 +1,15 @@
+const validPassword = () => <h1>valid Password</h1>
+const invalidPassword = () => <h1>invalid PasswordvalidPassword</h1>
+const Password = () =>{
+
+}
+
+
+
+
+
+
+
+
+
+export default valid;

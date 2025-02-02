54ffc8c759fcd1892370e3bcd7b04e5b93cf8cad
DOMINIC KALUMBA <dominiquekalumba1000@gmail.com>
2025-02-02 17:29:10 +0300

    react

--- src/valid.js
+++ src/valid.js
@@ -1,10 +1,14 @@
 const validPassword = () => <h1>valid Password</h1>
 const invalidPassword = () => <h1>invalid PasswordvalidPassword</h1>
-const Password = () =>{
+const Password = () =>{return {validPassword}
+{invalidPassword}
 
 }
 
+const App = () =>{
+    return <Password isvalid = {true}/>
 
+}
 
 
 
